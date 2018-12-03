9dd8c421f182851f33bb0dcdcda5a6fc1607bed3
Xu Han <xh2379@columbia.edu>
2018-12-03 18:22:03 -0500

    run

--- spidering/chinese/run_iqiyi.py
+++ spidering/chinese/run_iqiyi.py
@@ -2,25 +2,27 @@ import common
 import sys
 from iqiyi import get_responses
 
-num = 160
+num = 200
 sources = ('', 'iqiyi', 'qq', 'sohu', 'youku', 'tudou', 'acfun', 'bilibili', 'ifeng', 'cntv', 'm1905')
-url_filename = 'urls.txt'
-desc_filename = 'desc.txt'
-title_filename = 'titles.txt'
 
 # keyword = 'alphago'
 # output_dir = 'AlphaGo'
+# source = 2
 keyword = sys.argv[1]
 output_dir = sys.argv[2]
-for source in (2, 7):
-    print('Video source: %s' % sources[source])
-    data_filename = 'data_iqiyi_' + sources[source]
+source = int(sys.argv[3])
 
-    ps = get_responses(keyword, num, duration=1, source=source)
-    urls = [p[0] for p in ps]
-    common.save_urls(urls, url_filename, 'a')
-    videos = common.get_videos_info(ps)
-    common.save_data(videos, data_filename)
-    common.save_des(videos, desc_filename, 'a')
-    common.save_titles(videos, title_filename, 'a')
+url_filename = 'urls_' + sources[source] + '.txt'
+desc_filename = 'desc_' + sources[source] + '.txt'
+title_filename = 'titles_' + sources[source] + '.txt'
+data_filename = 'data_' + sources[source]
+
+ps = get_responses(keyword, num, duration=1, source=source)
+urls = [p[0] for p in ps]
+common.save_urls(urls, url_filename, 'a')
+videos = common.get_videos_info(ps)
+common.save_data(videos, data_filename)
+common.save_des(videos, desc_filename, 'a')
+common.save_titles(videos, title_filename, 'a')
+
 common.download(url_filename, output_dir)
