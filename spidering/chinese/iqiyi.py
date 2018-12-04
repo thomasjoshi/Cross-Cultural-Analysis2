cfb050f13c82d96b5f5ddbed2119a841606c3cc3
Xu Han <xh2379@columbia.edu>
2018-12-04 14:41:37 -0500

    spidering

--- spidering/chinese/iqiyi.py
+++ spidering/chinese/iqiyi.py
@@ -10,7 +10,8 @@ def get_responses(keyword, num, duration=0, source=0):
     result = []
     page = 1
     while len(result) < num:
-        s = 'https://so.iqiyi.com/so/q_' + keyword + '_ctg__t_' + str(durations[duration]) + '_page_' + str(page) + '_p_1_qc_0_rd__site_' + sources[source] + '_m_1_bitrate_'
+        s = 'https://so.iqiyi.com/so/q_' + keyword + '_ctg__t_' + str(durations[duration]) + '_page_' + str(
+            page) + '_p_1_qc_0_rd__site_' + sources[source] + '_m_1_bitrate_'
         response = requests.get(s)
         soup = BeautifulSoup(response.text, 'html.parser')
         video_items = soup.find_all('li', {'class': 'list_item'})
@@ -37,4 +38,7 @@ def parse(p):
             break
     d = json.loads(l)
     poster = d['user']['name'] if 'user' in d else None
-    return {'url': url, 'title': d['name'], 'description': d['description'], 'duration': d['duration'], 'publish_time': datetime.fromtimestamp(d['firstPublishTime'] * 1e-3), 'last_time': datetime.fromtimestamp(d['lastPublishTime'] * 1e-3), 'poster': poster, 'id': d['vid'], 'subtitle': d['subtitle']}
+    return {'url': url, 'title': d['name'], 'description': d['description'], 'duration': d['duration'],
+            'publish_time': datetime.fromtimestamp(d['firstPublishTime'] * 1e-3),
+            'last_time': datetime.fromtimestamp(d['lastPublishTime'] * 1e-3), 'poster': poster, 'id': d['vid'],
+            'subtitle': d['subtitle']}
