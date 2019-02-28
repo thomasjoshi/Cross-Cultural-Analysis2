2aaa83c50a8b414d565f9b7c05af28c1beaaa2c9
DandilionLau <incredibleup@gmail.com>
2019-02-28 02:04:11 -0500

    minor fix

--- dataset/transcribe.py
+++ dataset/transcribe.py
@@ -20,8 +20,10 @@ audio_type = opt.audio_format
 
 if opt.folder == "english":
     mode = 'en-US'
+    hint = ['AlphaGo', 'Lee Sedol', 'Ke Jie']
 elif opt.folder == 'chinese':
     mode = 'cmn-Hans-CN'
+    hint = ['AlphaGo','AlphaZero', 'DeepMind']
 else:
     print('Error: Need to specify the language for google speech recognition...')
 
@@ -46,6 +48,9 @@ class Transcriber:
             encoding='FLAC',
             language_code= mode,
             enable_word_time_offsets=True,
+            speech_contexts=[types.SpeechContext(
+            phrases=hint,
+           )],
             enable_word_confidence=True
         )
         operation = self.client.long_running_recognize(config=config, audio=audio)
@@ -55,6 +60,7 @@ class Transcriber:
             if len(alternatives) == 0:
                 continue
             alternative = alternatives[0]
+            results.append([alternative.transcript, alternative.confidence])
             for word_info in alternative.words:
                 word = word_info.word
                 start_time = word_info.start_time.seconds + round(word_info.start_time.nanos * 1e-9, 1)
@@ -79,6 +85,7 @@ class Transcriber:
 
 if __name__ == '__main__':
     transcriber = Transcriber()
+    f_log = open('log.txt', 'w')
 
     a_list = []
     for (dirpath, dirnames, filenames) in walk(audio_path):
@@ -89,18 +96,39 @@ if __name__ == '__main__':
             print('===> Start uploding file: ' + audio_filename)
             gs_uri = transcriber.upload_to_gcs(audio_path + '/' + audio_filename)
             print('===> Finish uploading file: ' + audio_filename)
-            results = transcriber.translate_with_timestamps(gs_uri)
 
-            with open(transcript_path + '/' + audio_filename[:-5] + '.txt', 'w') as f:
-                for item in results:
-                    word,start_time,end_time,confidence  = item[0], item[1], item[2], item[3]
-                    if opt.folder == 'english':
-                        #f.write(word + '\t' + str(start_time) + '\t' + str(end_time) + '\t' + str(confidence) + '\n')
-                        f.write(word+"\t{}\t{}\t{}\n".format((start_time,end_time,confidence)))
-                    else:
-                        f.write((word.encode('utf-8')+"\t{}\t{}\t{}\n".format(start_time,end_time,confidence)))
-                        #f.write(word.encode('utf-8') + '\t' + str(start_time) + '\t' + end_time + '\t' + confidence + '\n')
-            f.close()
+            try:
+                results = transcriber.translate_with_timestamps(gs_uri)
+                with open(transcript_path + '/' + audio_filename[:-5] + '.txt', 'w') as f:
+                    for item in results:
+                        if len(item) == 4:
+                            word,start_time,end_time,confidence  = item[0],item[1], item[2], item[3]
+                            if opt.folder == 'english':
+                                f.write((word+"\t{}\t{}\t{}\n".format(start_time,end_time,confidence)))
+                            else:
+                                f.write((word.encode('utf-8')+"\t{}\t{}\t{}\n".format(start_time,end_time,confidence)))
+                        else:
+                            trans,confidence = item[0], item[1]
+                            if opt.folder == 'english':
+                                f.write((trans+"\t{}\n".format(confidence)))
+                                print("--------------------------- transcript ---------------------------")
+                                print(trans)
+                            else:
+                                f.write((trans.encode('utf-8')+"\t{}\n".format(confidence)))
+                                print("--------------------------- transcript ---------------------------")
+                                print(trans.encode('utf-8'))
+
+                            print("--------------------------- confidence ---------------------------")
+                            print(confidence)
+                            print("--------------------------- confidence ---------------------------")
+                            f.write("\n")
+                f.close()
+
+            except Exception as e:
+                print("===> Error: writing to log.txt...")
+                f_log.write(audio_filename)
+                f_log.write(str(e))
+                f_log.write('\n')
 
             print('===> Delete file: ' + audio_filename)
             transcriber.delete_from_gcs(audio_filename)
