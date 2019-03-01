8835071e61bb3652878d98fc54a4aac35cdb0741
DandilionLau <incredibleup@gmail.com>
2019-03-01 02:17:03 -0500

    minor update

--- dataset/sample.py
+++ dataset/sample.py
@@ -3,6 +3,7 @@ import math
 import os
 import argparse
 from os import walk
+import numpy as np
 
 parser = argparse.ArgumentParser(description='extract_frame: extract frames and words')
 parser.add_argument('--folder', type=str, default="english", help='sub folder in dataset')
@@ -10,6 +11,7 @@ parser.add_argument('--dataset', type=str, default="AlphaGo", help='name of the 
 parser.add_argument('--video_format', type=str, default=".mp4", help='format of input videos')
 parser.add_argument('--transcript_format', type=str, default=".txt", help='format of output audios')
 parser.add_argument('--interval', type=int, default=5, help='length of sampling interval')
+parser.add_argument('--threshold', type=float, default=150, help='treshold of inter frame differences')
 opt = parser.parse_args()
 
 dataset_name = "./" + opt.dataset
@@ -38,12 +40,11 @@ for (dirpath, dirnames, filenames) in walk(trans_path):
 sample_interval = opt.interval
 
 for index,trans_file in enumerate(f_list):
+    print(u'Now processing: {}'.format(trans_file))
     samples = []
     start_time = []
-    confidence = []
     end_time = []
     word_buffer = []
-    phrase_buffer = []
 
     video_file = trans_file[:-len(opt.transcript_format)] + video_type
     full_video_path = video_path + "/" + video_file
@@ -57,7 +58,7 @@ for index,trans_file in enumerate(f_list):
                 word_buffer.append(words[0])
                 start_time.append(float(words[1]))
                 end_time.append(float(words[2]))
-                confidence.append(float(words[3]))
+                #confidence.append(float(words[3]))
 
         traverse_index = 0
         old_traverse_index = traverse_index
@@ -85,10 +86,11 @@ for index,trans_file in enumerate(f_list):
 
         print("===> Total number of interval: " + str(len(samples)))
 
-
+        '''
         for sample in samples:
             print(sample[2].decode('utf-8').encode('utf-8'))
         exit()
+        '''
 
         capture = cv2.VideoCapture(full_video_path)
         frameRate = capture.get(5) # get frame rate
@@ -96,41 +98,48 @@ for index,trans_file in enumerate(f_list):
         idx = 0
         count = 0
         is_ready = True
+        last_frame = -1
 
         while(capture.isOpened() and idx < len(samples)):
             frameId = capture.get(1)
             ret, frame = capture.read()
 
+            if frameId == last_frame:
+                break
+            else:
+                last_frame = frameId
+
             if (frameId <= samples[idx][1] * frameRate and frameId >= samples[idx][0] * frameRate):
                 is_ready = False
                 if frameId % round(frameRate) == 0:
+                #if np.sum( np.absolute(frame-last_frame) )/np.size(frame) > opt.threshold:
                     out_filename = '_'.join([str(int(samples[idx][0])),str(int(samples[idx][1]))])
                     out_img_filename = out_frame_path + "/" + trans_file[:-len(opt.transcript_format)] + "/" + out_filename + "_" + str(count) + ".jpg"
                     if not os.path.exists(out_frame_path + "/" + trans_file[:-len(opt.transcript_format)]):
                         os.makedirs(out_frame_path + "/" + trans_file[:-len(opt.transcript_format)])
                     cv2.imwrite(out_img_filename, frame)
                     count += 1
-                    print("Write frame: " + out_img_filename)
+                    print("Writing frame: " + out_img_filename)
 
-            if not is_ready and frameId > samples[idx][1] * frameRate:
-                out_trans_filename = out_text_path + "/" + trans_file[:-len(opt.transcript_format)] + "/" + out_filename + ".txt"
-                if not os.path.exists(out_text_path + "/" + trans_file[:-len(opt.transcript_format)]):
-                    os.makedirs(out_text_path + "/" + trans_file[:-len(opt.transcript_format)])
-                fw = open(out_trans_filename, 'w')
-                if opt.folder == "english":
-                    fw.write(samples[idx][2]+'\n')
-                else:
-                    fw.write(samples[idx][2].decode('utf-8').encode('utf-8')+'\n')
-                fw.close()
+            if (not is_ready and frameId > samples[idx][1] * frameRate):
+                try:
+                    out_trans_filename = out_text_path + "/" + trans_file[:-len(opt.transcript_format)] + "/" + out_filename + ".txt"
+                    if not os.path.exists(out_text_path + "/" + trans_file[:-len(opt.transcript_format)]):
+                        os.makedirs(out_text_path + "/" + trans_file[:-len(opt.transcript_format)])
+                    fw = open(out_trans_filename, 'w')
+                    if opt.folder == "english":
+                        fw.write(samples[idx][2]+'\n')
+                    else:
+                        fw.write(samples[idx][2].decode('utf-8').encode('utf-8')+'\n')
+                    fw.close()
+                except NameError:
+                    print("No keyframe above threshold found...")
+
                 count = 0
                 is_ready = True
 
-            while frameId > samples[idx][0] * frameRate and is_ready:
-                #print(idx,frameId, samples[idx][0])
+            while idx < len(samples) and frameId > samples[idx][0] * frameRate and is_ready:
                 idx += 1
-                if idx >= len(samples):
-                    break
-
 
         capture.release()
         print ("Finish processing file: " + full_video_path)
