d333131f9947078b08155bef9a71ce6e37d5fc91
zyyhhxx <yz3645@columbia.edu>
2020-10-13 01:05:22 -0400

    Incorporate separate function calls into run.py

---
+++ run.py
@@ -0,0 +1,30 @@
+import argparse
+from src.spidering.get_metadata import get_metadata
+from src.spidering.download import download
+from src.tool.profiler import profile
+
+
+def main():
+    """
+    The main function of the project
+    """
+    parser = argparse.ArgumentParser(description='Cross Cultural Analysis')
+    parser.add_argument(
+        '-k', '--key', help='key for Youtube API, string or path to text file')
+    args = parser.parse_args()
+
+    # Get video metadata
+    profile(get_metadata, ("Modi NDTV", 10,
+                           "results/Modi/NDTV/video_metadata", False, args.key, ["youtube"]))
+    profile(get_metadata, ("Modi Republic World", 10,
+                           "results/Modi/republic_world/video_metadata", False, args.key, ["youtube"]))
+
+    # Download videos
+    profile(download, ("results/Modi/NDTV/video_metadata/metadata",
+                       "results/Modi/NDTV/audios", "results/Modi/NDTV/videos", "youtube"))
+    profile(download, ("results/Modi/republic_world/video_metadata/metadata",
+                       "results/Modi/republic_world/audios", "results/Modi/republic_world/videos", "youtube"))
+
+
+if __name__ == '__main__':
+    main()
